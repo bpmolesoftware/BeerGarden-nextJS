0fc30a4454ddc62565f74826e5b45887fc3821c3
bpmolesoftware <bp@molesoftware.com>
2023-11-08 09:32:29 +0200

    Add date and time
    Added date and time to comments

--- src/pages/api/comments/addComments.js
+++ src/pages/api/comments/addComments.js
@@ -2,9 +2,16 @@ import { createConnectionToDB } from '../connectDB';
 
 const myfunc = async (req, res) => {
   const dbconnection = await createConnectionToDB();
+  const today = new Date();
   try {
+    const date = `${today.getFullYear()}-${
+      today.getMonth() + 1
+    }-${today.getDate()}`;
+    const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+    const dateTime = `${date} ${time}`;
+
     const { name, id, comment } = JSON.parse(JSON.stringify(req.body));
-    const query = `INSERT INTO Comments (name, commentText, beergardenId) values ("${name}", "${comment}", ${id})`;
+    const query = `INSERT INTO Comments (name, commentText, beergardenId, dateAndTime) values ("${name}", "${comment}", ${id}, "${date} ${time}")`;
 
     const values = [];
     const [data] = await dbconnection.execute(query, values);
