c15d2d90eb56865e8c8048e6f046512cbcc24fcc
bpmolesoftware <bp@molesoftware.com>
2023-11-28 13:35:32 +0200

    try to connect to planetscale
    try to connect to planetscale via vercel

--- src/pages/api/connectDB.js
+++ src/pages/api/connectDB.js
@@ -1,14 +1,14 @@
 import mysql from 'mysql2/promise';
 
-const host = process.env.HOST;
-const database = process.env.DATABASE;
-const user = process.env.USER;
-const password = process.env.PASSWORD;
+const host = process.env.DATABASE_HOST;
+//const database = process.env.DATABASE;
+const user = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
 
 const createConnectionToDB = async () => {
   const connectToDB = await mysql.createConnection({
     host,
-    database,
+    //database,
     user,
     password,
   });
