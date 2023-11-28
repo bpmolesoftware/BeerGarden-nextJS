857c61fb57989b260050a1c7f14307365708937b
bpmolesoftware <bp@molesoftware.com>
2023-11-28 16:15:57 +0200

    add planetscale env

--- src/pages/api/connectDB.js
+++ src/pages/api/connectDB.js
@@ -1,16 +1,11 @@
 import mysql from 'mysql2/promise';
 const fs = require("fs");
 
-// const host = process.env.PLANETSCALE_DB_HOST;
-// const database = process.env.PLANETSCALE_DB;
-// const user = process.env.PLANETSCALE_DB_USERNAME;
-// const password = process.env.PLANETSCALE_DB_PASSWORD;
-// const sslCert = process.env.PLANETSCALE_SSL_CERT_PATH;
-
-const host = process.env.HOST;
-const database = process.env.DATABASE;
-const user = process.env.USER;
-const password = process.env.PASSWORD;
+const host = process.env.PLANETSCALE_DB_HOST;
+const database = process.env.PLANETSCALE_DB;
+const user = process.env.PLANETSCALE_DB_USERNAME;
+const password = process.env.PLANETSCALE_DB_PASSWORD;
+const sslCert = process.env.PLANETSCALE_SSL_CERT_PATH;
 
 const createConnectionToDB = async () => {
   const connectToDB = await mysql.createConnection({
@@ -18,7 +13,9 @@ const createConnectionToDB = async () => {
     database,
     user,
     password,
-   
+    ssl: {
+      ca: fs.readFileSync(sslCert)
+  }
   });
   return connectToDB;
 };
